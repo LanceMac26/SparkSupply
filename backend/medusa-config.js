1c0084af60d4d3fab0d1caf60bdca155db455376
LanceMac26 <lancemac26@gmail.com>
2024-12-28 22:57:38 +0200

    Minor config updates

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -19,7 +19,12 @@ import {
   MINIO_ENDPOINT,
   MINIO_ACCESS_KEY,
   MINIO_SECRET_KEY,
-  MINIO_BUCKET
+  MINIO_BUCKET,
+  PAYPAL_SANDBOX,
+  PAYPAL_CLIENT_ID,
+  PAYPAL_CLIENT_SECRET,
+  PAYPAL_AUTH_WEBHOOK_ID,
+  PAYPAL_BASE_URL,
 } from 'lib/constants';
 
 loadEnv(process.env.NODE_ENV, process.cwd());
@@ -35,8 +40,8 @@ const medusaConfig = {
       authCors: AUTH_CORS,
       storeCors: STORE_CORS,
       jwtSecret: JWT_SECRET,
-      cookieSecret: COOKIE_SECRET
-    }
+      cookieSecret: COOKIE_SECRET,
+    },
   },
   admin: {
     backendUrl: BACKEND_URL,
@@ -48,86 +53,133 @@ const medusaConfig = {
       resolve: '@medusajs/file',
       options: {
         providers: [
-          ...(MINIO_ENDPOINT && MINIO_ACCESS_KEY && MINIO_SECRET_KEY ? [{
-            resolve: './src/modules/minio-file',
-            id: 'minio',
-            options: {
-              endPoint: MINIO_ENDPOINT,
-              accessKey: MINIO_ACCESS_KEY,
-              secretKey: MINIO_SECRET_KEY,
-              bucket: MINIO_BUCKET // Optional, default: medusa-media
-            }
-          }] : [{
-            resolve: '@medusajs/file-local',
-            id: 'local',
-            options: {
-              upload_dir: 'static',
-              backend_url: `${BACKEND_URL}/static`
-            }
-          }])
-        ]
-      }
-    },
-    ...(REDIS_URL ? [{
-      key: Modules.EVENT_BUS,
-      resolve: '@medusajs/event-bus-redis',
-      options: {
-        redisUrl: REDIS_URL
-      }
-    },
-    {
-      key: Modules.WORKFLOW_ENGINE,
-      resolve: '@medusajs/workflow-engine-redis',
-      options: {
-        redis: {
-          url: REDIS_URL,
-        }
-      }
-    }] : []),
-    ...(SENDGRID_API_KEY && SENDGRID_FROM_EMAIL || RESEND_API_KEY && RESEND_FROM_EMAIL ? [{
-      key: Modules.NOTIFICATION,
-      resolve: '@medusajs/notification',
-      options: {
-        providers: [
-          ...(SENDGRID_API_KEY && SENDGRID_FROM_EMAIL ? [{
-            resolve: '@medusajs/notification-sendgrid',
-            id: 'sendgrid',
-            options: {
-              channels: ['email'],
-              api_key: SENDGRID_API_KEY,
-              from: SENDGRID_FROM_EMAIL,
-            }
-          }] : []),
-          ...(RESEND_API_KEY && RESEND_FROM_EMAIL ? [{
-            resolve: './src/modules/email-notifications',
-            id: 'resend',
-            options: {
-              channels: ['email'],
-              api_key: RESEND_API_KEY,
-              from: RESEND_FROM_EMAIL,
-            },
-          }] : []),
-        ]
-      }
-    }] : []),
-    ...(STRIPE_API_KEY && STRIPE_WEBHOOK_SECRET ? [{
-      key: Modules.PAYMENT,
-      resolve: '@medusajs/payment',
-      options: {
-        providers: [
-          {
-            resolve: '@medusajs/payment-stripe',
-            id: 'stripe',
-            options: {
-              apiKey: STRIPE_API_KEY,
-              webhookSecret: STRIPE_WEBHOOK_SECRET,
-            },
-          },
+          ...(MINIO_ENDPOINT && MINIO_ACCESS_KEY && MINIO_SECRET_KEY
+            ? [
+                {
+                  resolve: './src/modules/minio-file',
+                  id: 'minio',
+                  options: {
+                    endPoint: MINIO_ENDPOINT,
+                    accessKey: MINIO_ACCESS_KEY,
+                    secretKey: MINIO_SECRET_KEY,
+                    bucket: MINIO_BUCKET, // Optional, default: medusa-media
+                  },
+                },
+              ]
+            : [
+                {
+                  resolve: '@medusajs/file-local',
+                  id: 'local',
+                  options: {
+                    upload_dir: 'static',
+                    backend_url: `${BACKEND_URL}/static`,
+                  },
+                },
+              ]),
         ],
       },
-    }] : [])
+    },
+    ...(REDIS_URL
+      ? [
+          {
+            key: Modules.EVENT_BUS,
+            resolve: '@medusajs/event-bus-redis',
+            options: {
+              redisUrl: REDIS_URL,
+            },
+          },
+          {
+            key: Modules.WORKFLOW_ENGINE,
+            resolve: '@medusajs/workflow-engine-redis',
+            options: {
+              redis: {
+                url: REDIS_URL,
+              },
+            },
+          },
+        ]
+      : []),
+    ...((SENDGRID_API_KEY && SENDGRID_FROM_EMAIL) ||
+    (RESEND_API_KEY && RESEND_FROM_EMAIL)
+      ? [
+          {
+            key: Modules.NOTIFICATION,
+            resolve: '@medusajs/notification',
+            options: {
+              providers: [
+                ...(SENDGRID_API_KEY && SENDGRID_FROM_EMAIL
+                  ? [
+                      {
+                        resolve: '@medusajs/notification-sendgrid',
+                        id: 'sendgrid',
+                        options: {
+                          channels: ['email'],
+                          api_key: SENDGRID_API_KEY,
+                          from: SENDGRID_FROM_EMAIL,
+                        },
+                      },
+                    ]
+                  : []),
+                ...(RESEND_API_KEY && RESEND_FROM_EMAIL
+                  ? [
+                      {
+                        resolve: './src/modules/email-notifications',
+                        id: 'resend',
+                        options: {
+                          channels: ['email'],
+                          api_key: RESEND_API_KEY,
+                          from: RESEND_FROM_EMAIL,
+                        },
+                      },
+                    ]
+                  : []),
+              ],
+            },
+          },
+        ]
+      : []),
+    ...((STRIPE_API_KEY && STRIPE_WEBHOOK_SECRET) ||
+    (PAYPAL_CLIENT_ID && PAYPAL_CLIENT_SECRET)
+      ? [
+          {
+            key: Modules.PAYMENT,
+            resolve: '@medusajs/payment',
+            options: {
+              providers: [
+                ...(STRIPE_API_KEY && STRIPE_WEBHOOK_SECRET
+                  ? [
+                      {
+                        resolve: '@medusajs/payment-stripe',
+                        id: 'stripe',
+                        options: {
+                          apiKey: STRIPE_API_KEY,
+                          webhookSecret: STRIPE_WEBHOOK_SECRET,
+                        },
+                      },
+                    ]
+                  : []),
+                ...(PAYPAL_CLIENT_ID && PAYPAL_CLIENT_SECRET
+                  ? [
+                      {
+                        resolve: './src/modules/paypal-payment',
+                        id: 'paypal',
+                        options: {
+                          sandbox: PAYPAL_SANDBOX,
+                          clientId: PAYPAL_CLIENT_ID,
+                          clientSecret: PAYPAL_CLIENT_SECRET,
+                          authWebhookId: PAYPAL_AUTH_WEBHOOK_ID,
+                          baseURL: PAYPAL_BASE_URL,
+                        },
+                      },
+                    ]
+                  : []),
+              ],
+            },
+          },
+        ]
+      : []),
   ],
-  plugins: []
+  plugins: [],
 };
 
 console.log(JSON.stringify(medusaConfig, null, 2));
